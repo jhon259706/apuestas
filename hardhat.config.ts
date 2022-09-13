d76a5a546e80d92db08be45e7dc4e4ade8289f85
Jhon Melendez <jmelendez@saatvamattress.com>
2022-09-13 08:46:53 -0500

    Adding rinkeby deployment configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
+
+const projectId: string = process.env.INFURA_PROJECT_ID ?? '';
+const privateKey: string = process.env.DEPLOYER_SIGNER_PRIVATE_KEY ?? '';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts: [
+        privateKey
+      ],
+    },
+  },
 };
 
 export default config;
